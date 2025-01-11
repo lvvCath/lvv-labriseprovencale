a45494c358dcd1ef649c48b8e74d59f53f3c365d
lvvCath <catherinejustinezuniga@gmail.com>
2025-01-12 02:06:40 +0800

    the villa update
    - ongoing section 2

--- src/pages/the_villa/Section2.tsx
+++ src/pages/the_villa/Section2.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "./Section2.css";
 import GetText from "../../components/TextExtractor";
 import { useLanguage } from "../../components/LanguageContext";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import divider from "../../assets/icon/divider1.png";
 
 function Section2() {
   const { language } = useLanguage();
@@ -18,55 +19,89 @@ function Section2() {
   ];
 
   return (
-    <Container fluid className="premium-suite-section">
-      <div className="navigation">
-        <div className="nav-title-container">
-          <hr className="nav-line" />
-          <h2 className="nav-title">Premium Suite</h2>
-          <hr className="nav-line" />
-        </div>
+    <Container fluid className="thevilla-s2">
+      <Row className="navigation-s2">
+        <Col>
+          <h1 className="header-text-light s4-title">
+            <GetText
+              folder={language}
+              page="thevilla"
+              section="section2"
+              field="title"
+            />
+          </h1>
+        </Col>
+        <Col className="nav_container_s2" lg={{ order: "first" }}>
+          <Row>
+            <span className="nav-line"></span>
+          </Row>
+          {navItems.slice(0, 3).map((item) => (
+            <button
+              key={item.id}
+              className={`nav-button underline-effect ${
+                selectedNav === item.id ? "active" : ""
+              }`}
+              onClick={() => setSelectedNav(item.id)}
+            >
+              <p className="text-color-light nav-text-s2">{item.label}</p>
+            </button>
+          ))}
+        </Col>
 
-        <div className="nav-buttons-container">
-          <div className="nav-buttons">
-            {navItems.slice(0, 3).map((item) => (
-              <button
-                key={item.id}
-                className={`nav-button ${
-                  selectedNav === item.id ? "active" : ""
-                }`}
-                onClick={() => setSelectedNav(item.id)}
-              >
-                {item.label}
-              </button>
-            ))}
-          </div>
-          <div className="nav-buttons">
-            {navItems.slice(3).map((item) => (
-              <button
-                key={item.id}
-                className={`nav-button ${
-                  selectedNav === item.id ? "active" : ""
-                }`}
-                onClick={() => setSelectedNav(item.id)}
-              >
-                {item.label}
-              </button>
-            ))}
-          </div>
-        </div>
-      </div>
+        <Col className="nav_container_s2">
+          <Row>
+            <span className="nav-line"></span>
+          </Row>
+          {navItems.slice(3).map((item) => (
+            <button
+              key={item.id}
+              className={`nav-button underline-effect ${
+                selectedNav === item.id ? "active" : ""
+              }`}
+              onClick={() => setSelectedNav(item.id)}
+            >
+              <p className="text-color-light nav-text-s2">{item.label}</p>
+            </button>
+          ))}
+        </Col>
+      </Row>
 
       <div className="content-section">
-        <div className="content">
-          <p className="body-text-dark">
+        {/* Room Image */}
+        <img
+          className="img-title-s2"
+          src={`src/assets/images/thevilla/section2x1x${selectedNav}.jpeg`}
+          alt="profile image"
+        />
+
+        {/* Room Title */}
+        <div className="content-title-container">
+          <h1 className="header-text-dark title">
             <GetText
               folder={language}
               page="thevilla"
               section="section2"
               field={selectedNav}
             />
+          </h1>
+          <p className="body-text-dark subtitle">
+            <GetText
+              folder={language}
+              page="thevilla"
+              section="section2"
+              field={`${selectedNav}xsubtitle`}
+            />
           </p>
+          <img src={divider} alt="Divider" className="divider-image" />
         </div>
+
+        {/* Room Details */}
+        <Row>
+          {/* Images */}
+          <Col></Col>
+          {/* Info */}
+          <Col></Col>
+        </Row>
       </div>
     </Container>
   );
