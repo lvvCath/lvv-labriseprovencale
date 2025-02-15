d7305629f9acfc7806c801548a4ead38b442936f
lvvCath <catherinejustinezuniga@gmail.com>
2025-02-15 20:59:10 +0800

    update

--- src/components/TextExtractor.tsx
+++ src/components/TextExtractor.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 
 interface TextExtractorProps {
   folder: string;
@@ -14,16 +13,8 @@ function GetText({ folder, page, section, field }: TextExtractorProps) {
   useEffect(() => {
     async function fetchText() {
       try {
-        const response = await fetch(
-          `/assets/translations/${folder}/${page}.json`
-        );
-
-        if (!response.ok) {
-          throw new Error(`Failed to load ${page}.json`);
-        }
-
-        const data = await response.json();
-        setText(data?.[section]?.[field] || null);
+        const response = await import(`../translations/${folder}/${page}.json`);
+        setText(response[section][field]);
       } catch (error) {
         console.error(
           `Error loading text content for ${page}, section ${section}, field ${field}:`,
@@ -37,18 +28,14 @@ function GetText({ folder, page, section, field }: TextExtractorProps) {
   }, [folder, page, section, field]);
 
   return (
-    <motion.span
-      initial={{ opacity: 0, y: 5 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-    >
+    <>
       {text?.split("\n").map((line, index) => (
         <span key={index}>
           {line}
           {index < text.split("\n").length - 1 && <br />}
         </span>
       ))}
-    </motion.span>
+    </>
   );
 }
 
