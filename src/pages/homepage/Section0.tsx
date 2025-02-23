26ab413a17aa2aa53363378e712a6156f1135ad7
lvvCath <catherinejustinezuniga@gmail.com>
2025-02-24 00:21:27 +0800

    homepage logo update

--- src/pages/homepage/Section0.tsx
+++ src/pages/homepage/Section0.tsx
@@ -1,5 +1,6 @@
 import "./Section0.css";
 import HomepageVideo from "/assets/video/Homepage.mp4";
+import Logo from "/assets/icon/logo.png"; // Import the logo
 
 function Section0() {
   return (
@@ -8,6 +9,7 @@ function Section0() {
         <source src={HomepageVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <img src={Logo} alt="Logo" className="video-logo" /> {/* Add logo */}
     </div>
   );
 }
